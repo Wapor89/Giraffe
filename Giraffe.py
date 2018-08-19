5db5e43adac073f70546627fa19c76939b611446
wapor89 <elvisolli123Github#>
2018-08-19 09:47:20 +0200

    Setup vcs

---
+++ Giraffe.py
@@ -0,0 +1,13 @@
+character_name = "John"
+character_age = 35.55
+is_male = False
+
+phrase = "Text"
+phrase = "There once was a man named " + character_name + ", "
+phrase += "he was " + character_age.__str__() + " years old. "
+phrase += "He really liked the name " + character_name + ", "
+phrase += "but didn't like being " + character_age.__str__() + "."
+
+print(phrase.capitalize())
+
+newline = "Test"
